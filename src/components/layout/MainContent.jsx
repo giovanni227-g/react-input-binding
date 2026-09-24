f33e313bd1b1b2a668be74223171d831fe290be0
giovanni227-g <giovanni.zattera@gmail.com>
2026-09-24 20:56:43 +0200

    Improve layout and styling

--- src/components/layout/MainContent.jsx
+++ src/components/layout/MainContent.jsx
@@ -1,9 +1,17 @@
 import FilterName from '../sections/FilterName.jsx'
+import HelloControlled from '../sections/HelloControlled.jsx'
 
 export default function MainContent() {
   return (
     <main className="py-5">
-      <FilterName />
+      <div className="container app-grid">
+        <div className="app-panel">
+          <HelloControlled />
+        </div>
+        <div className="app-panel">
+          <FilterName />
+        </div>
+      </div>
     </main>
   )
 }
